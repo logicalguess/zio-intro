9f958fb5b85af135ef4843006dbf524200c982f5
logicalguess <logicalguess@gmail.com>
2019-10-12 22:06:48 -0400

    retry

--- src/main/scala/logicalguess/zio/app/Recommender.scala
+++ src/main/scala/logicalguess/zio/app/Recommender.scala
@@ -1,7 +1,8 @@
 package logicalguess.zio.app
 
+import zio.clock.Clock
 import zio.console.Console
-import zio.{DefaultRuntime, Ref, ZIO, console}
+import zio.{DefaultRuntime, Ref, Schedule, ZIO, console}
 
 // based on http://cloudmark.github.io/A-Journey-To-Zio/
 
@@ -40,27 +41,27 @@ object Recommender {
       case _ => log(s"LOG -> User $email is not valid") *> ZIO.fail(s"ERROR -> User $email not found")
     }
 
-  def tryGettingRecommendationsByEmail(email: String): ZIO[Writer[String], Error, Recommendations] =
+  def tryGettingRecommendationsByEmail(email: String): ZIO[Writer[String] with Clock, Error, Recommendations] =
     for {
       _         <- log(s"LOG -> Searching for user $email")
-      user      <- tryGettingUserByEmail(email)
+      user      <- tryGettingUserByEmail(email).retry(Schedule.recurs(2))
       _         <- log(s"LOG -> Getting recommendations for user $email")
       products  <- getRecommendationsById(user.id)
     } yield Recommendations(user.email, user.name, products)
 
-  def logic(email: String): ZIO[Console with Writer[String], Error, Recommendations] = for {
+  def logic(email: String): ZIO[Console with Writer[String] with Clock, Error, Recommendations] = for {
     recs <- tryGettingRecommendationsByEmail(email)//.ensuring(printLogs)
     _ <- printLogs // not reached without ensuring if error
   } yield (recs)
 
   def main(args: Array[String]): Unit = {
-    val email = "john@noreply.com"
+    val email = "johnx@noreply.com"
 
     val runtime = new DefaultRuntime{}
 
     val zio = for {
       w <- Ref.make[Log](Vector[String]())
-      live: Console with Writer[String] = new Console.Live with Writer[String] {
+      live: Console with Clock with Writer[String] = new Console.Live with Clock.Live with Writer[String] {
         def writer: Ref[Vector[String]] = w
       }
       result <- logic(email).provide(live).catchAllCause(c => console.putStrLn("" + c.failures(0)))
