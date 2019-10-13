792b53fe46cc76c5fe9ebcb16dd3c1aeda18c06c
logicalguess <logicalguess@gmail.com>
2019-10-13 19:14:06 -0400

    slides

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-ZIO Introduction
+![ ](./slides/zio-intro.001.png)
+
+![ ](./slides/zio-intro.002.png)
+
+![ ](./slides/zio-intro.003.png)
+
+![ ](./slides/zio-intro.004.png)
+
+![ ](./slides/zio-intro.005.png)
+
+![ ](./slides/zio-intro.006.png)
+
+![ ](./slides/zio-intro.007.png)
+
+![ ](./slides/zio-intro.008.png)
+
 
 Credits:
 
--- src/main/scala/logicalguess/zio/hello/HelloApp.scala
+++ src/main/scala/logicalguess/zio/hello/HelloApp.scala
@@ -2,16 +2,16 @@ package logicalguess.zio.hello
 
 import java.io.IOException
 
-import zio.console.{getStrLn, putStrLn}
+import zio.console.{Console, getStrLn, putStrLn}
 import zio.{App, ZIO}
 
 object HelloApp extends App {
 
-  def logic: ZIO[Environment,IOException, Unit] =
+  def logic: ZIO[Console,IOException, Unit] =
     for {
       _ <- putStrLn("What's your name?")
-      n <- getStrLn
-      _ <- putStrLn("Hello, " + n + "!")
+      name <- getStrLn
+      _ <- putStrLn("Hello, " + name + "!")
     } yield ()
 
   override def run(args: List[String]): ZIO[Environment, Nothing, Int] =
